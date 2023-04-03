f15875065a1c6129313666eba0d03b8823c69ca1
Jack Kilkenny <JohnKilkenny4@gmail.com>
2023-04-03 17:50:47 -0400

    first pass at scraping single tour

--- pga_scrape.py
+++ pga_scrape.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,15 +8,20 @@ r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
 
 #t = soup.find('h1', class_ = 'chakra-text css-axbyz4').text
-leader_table = soup.find('table', class_ = 'Table Table--align-right Full__Table')
-for player in leader_table.find_all('tbody'):
-    rows = player.find_all('tr')
+parsed_header = soup.find_all('table')[0].find('tr')
+header = []
+for i in parsed_header:
+    header.append(i.get_text())
+
+parsed_table = soup.find_all('table')[0].find_all('tr')[1:]
+data = []
+for rows in parsed_table:
+    init_df = []
     for row in rows:
-        pos = row.find('td', class_ = 'tl Table__TD').text
-        name = row.find('td', class_ = 'tl plyr Table__TD').text
-        f_score = row.find_all('td', class_ = 'Table__TD')[3].text
-        r1_score = row.find_all('td', class_ = 'Table__TD')[4].text,
-        r2_score = row.find_all('td', class_ = 'Table__TD')[5].text,
-        r3_score = row.find_all('td', class_ = 'Table__TD')[6].text,
-        r4_score = row.find_all('td', class_ = 'Table__TD')[7].text
-        print(pos, name, f_score, r1_score, r2_score, r3_score, r4_score)
+        init_df.append(row.get_text())
+    data.append(init_df)
+
+df = pd.DataFrame(data = data, columns=header).iloc[:,1:]
+
+print(df)
+
